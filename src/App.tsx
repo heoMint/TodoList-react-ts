d1f10dd6aa844947190e7a2e3ba9b4198af1c8ff
hdwmumu <wil0110@naver.com>
2024-02-29 16:48:23 +0900

    feat: ✨ delete기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,23 @@ function App() {
     // Todo의 데이터모델을 사용하는 객체배열
     // const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
     const [todos, setTodos] = useState<Todo[]>([]);
-    
 
     const addTodoHandler = (todoText: string) => {
         const newTodo = new Todo(todoText);
 
-        setTodos((prevTodos)=>{
-        // concat() 새로운 배열 생성
+        setTodos((prevTodos) => {
+            // concat() 새로운 배열 생성
             return prevTodos.concat(newTodo);
-        })
+        });
     };
+
+    const removeTodoHandler = (id: string) =>{
+        setTodos((prevTodos)=> prevTodos.filter((todo)=> todo.id !== id))
+    }
     return (
         <div>
             <NewTodo onAddTodo={addTodoHandler} />
-            <Todos items={todos} />
+            <Todos items={todos} onRemoveTodo={removeTodoHandler} />
         </div>
     );
 }
